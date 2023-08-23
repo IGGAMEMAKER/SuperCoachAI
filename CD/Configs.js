d75e281984367f0b282219dd4fc787641a654dd2
Gaga <23i03g@mail.ru>
2023-08-23 16:11:35 +0300

    GPT TEST

--- CD/Configs.js
+++ CD/Configs.js
@@ -1,5 +1,6 @@
 var confs = require('./Configs/confs.json');
 
 module.exports = {
-  TG_BOT_API_KEY: confs.TG_BOT_API_KEY
+  TG_BOT_API_KEY: confs.TG_BOT_API_KEY,
+  OPENAI_API_KEY: confs.OPENAI_API_KEY
 }
---
+++ experiments/gptTest.js
@@ -0,0 +1,23 @@
+const OpenAI = require('openai')
+const {OPENAI_API_KEY} = require("../CD/Configs");
+
+const openai = new OpenAI({
+  apiKey: OPENAI_API_KEY, // defaults to process.env["OPENAI_API_KEY"]
+});
+
+// async function main() {
+//   const completion = await openai.chat.completions.create({
+//     messages: [{ role: 'user', content: 'Say this is a test' }],
+//     model: 'gpt-3.5-turbo',
+//   });
+//
+//   console.log(completion.choices);
+// }
+
+// main()
+// .then(r => {
+//   console.log({r})
+// })
+// .catch(err => {
+//   console.error('failed to GPT', err)
+// })
